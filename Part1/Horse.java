c2afebc656f746196212b0f68ebe21bf3c957b5d
Bryan <bryan@SyntaxError.local>
2024-03-29 10:09:20 +0000

    added Horse Class fields

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,75 +2,35 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Bryan Piedra-Ojeda 230347627
+ * @version 1
  */
-public class Horse
-{
+public class Horse{
     //Fields of class Horse
-    
-    
-      
+    char horseSymbol;
+    String horseName;
+    double horseConfidence;
+    int DistanceTravelled;
+    boolean Fallen;
+
     //Constructor of class Horse
-    /**
-     * Constructor for objects of class Horse
-     */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
+    public Horse(char horseSymbol, String horseName, double horseConfidence){
+        this.horseSymbol = horseSymbol;
+        this.horseName= horseName;
+        this.horseConfidence = horseConfidence;
+        this.DistanceTravelled = 0;
+        this.Fallen = false;
     }
     
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
-    }
-
-    public void moveForward()
-    {
-        
-    }
-
-    public void setConfidence(double newConfidence)
-    {
-        
-    }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
-    }
-    
+    /*Methods*/
+    public void fall(){}
+    public double getConfidence(){}
+    public int getDistanceTravelled(){}
+    public String getName(){}
+    public char getSymbol(){}
+    public void goBackToStart(){}
+    public boolean hasFallen(){}
+    public void moveForward(){}
+    public void setConfidence(double newConfidence){}
+    public void setSymbol(char newSymbol){}
 }
