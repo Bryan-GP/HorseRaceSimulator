fb2c4a9fd338a05825fb92c3f2bdd104d96885f8
Bryan-GP <Bryan.gpiedra@gmail.com>
2024-04-19 13:27:55 +0100

    finished horse customiseation -> start track race and customissation

--- Part2/CustomiseHorsesWindow.java
+++ Part2/CustomiseHorsesWindow.java
@@ -1,38 +1,48 @@
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+
 import javax.swing.*;
 
 public class CustomiseHorsesWindow  {
-    //private static final char Symbol = 0
-    //JPanel panel 
-    //private JPanel panel = new JPanel(new GridLayout(horses.size(,2));
-    //private static int HorseCount;
     JFrame frame = new JFrame(); 
     private static double DefaultConfidence = 0.5;
     private static char DefaultSymbol = '\u265E';
-    private static ArrayList<JButton> horses; 
+    private static ArrayList<JButton> horses;
+    private static LinkedHashMap<Integer, Horse> ReadyHorses;
 
     public CustomiseHorsesWindow(int HorseCount){
         //CustomiseHorsesWindow.HorseCount = HorseCount;
         CustomiseHorsesWindow.horses = new ArrayList<>();
-        JPanel panel = new JPanel(new GridLayout(HorseCount,2));
-        for(int i=0; i<HorseCount; i++){ CustomiseHorsesWindow.horses.add(new JButton("Horse"+ (i+1))); }
+        CustomiseHorsesWindow.ReadyHorses = new LinkedHashMap<>();
+        JPanel panel = new JPanel(new GridLayout(HorseCount+1,1)); //added 1 for the submit button
+        for(int i=0; i<HorseCount; i++){
+            CustomiseHorsesWindow.horses.add(new JButton("Horse"+(i+1)));
+            CustomiseHorsesWindow.ReadyHorses.put(i,new Horse( DefaultSymbol, "Horse"+(i+1), DefaultConfidence) );
+        }
         for(int i=0; i<CustomiseHorsesWindow.horses.size(); i++){
             JButton b = CustomiseHorsesWindow.horses.get(i);
+            Horse h = CustomiseHorsesWindow.ReadyHorses.get(i);
             String windowName = "Horse "+(i+1);
             b.addActionListener(e -> {
                 if(e.getSource() == b){ 
                     frame.setVisible(false);
-                    //frame.dispose();
-                    char Symbol = DefaultSymbol;
-                    String DefaultName = b.getText();
-                    double Confidence = DefaultConfidence;
-                    new HorseWindow(this,windowName, b, new Horse( Symbol, DefaultName, Confidence));
+                    new HorseWindow( this, windowName, b, h );
                 }
             });
             panel.add(b);
         }
+        JButton SubmitButton = new JButton("START RACING");
+        SubmitButton.setBackground(new Color(333));
+        SubmitButton.addActionListener(e->{
+            if(e.getSource() == SubmitButton){
+                //frame.dispose();
+                new RaceGUI(ReadyHorses);
+            }
+        });
+        panel.add(SubmitButton);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Horse Racing Simulation");
