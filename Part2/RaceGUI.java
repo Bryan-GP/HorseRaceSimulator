e2eba9934dfc24df51027192461ed2748fe3b8f3
Bryan-GP <Bryan.gpiedra@gmail.com>
2024-04-20 00:38:09 +0100

    find a way to get all the Horses in Race to Race on the GUI

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -2,15 +2,24 @@ import java.util.LinkedHashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class RaceGUI {
     JFrame frame = new JFrame("HORSE RACING SIMULATOR");
-
     public RaceGUI(LinkedHashMap<Integer,Horse> horses ){
         JPanel panel = new JPanel();
 
-    }
-}//make this calss show the race with all the horses.
+        JTextField RaceOutput = new JTextField();
+        RaceOutput.setEditable(false);
+        
 
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 420);
+        frame.setVisible(true);
+    }
+}
+//make this calss show the race with all the horses.
 //in RacingGUI branch now
+//make a textfield and use that as the terminal.
